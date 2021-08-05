bba4adf90a219f9d1e7b76046344003086265236
ttt8000 <ttt8000@g.rit.edu>
2021-08-05 16:33:23 -0400

    Styling for browser

---
+++ target/classes/public/js/playerView.js
@@ -0,0 +1,42 @@
+
+export function createPlayerViewElement (player) {
+
+    const playerView = document.createElement("div")
+    playerView.setAttribute("class", "player_view")
+    playerView.setAttribute("id", player.name)
+
+
+    const cardView = document.createElement("div")
+    cardView.setAttribute("class", "card_view")
+    let hand = player.hand.cards;
+    hand.forEach(card => {
+        let card_name = card.id + "_" + card.suit
+        let card_img = document.createElement("img")
+        if (!card.flipped)
+            card_img.setAttribute("src", "hidden_card");
+        else
+            card_img.setAttribute("src", card_name);
+        card_img.setAttribute("alt", card_name)
+
+        cardView.appendChild(card_img)
+
+    });
+
+
+    const nameView = document.createElement("div")
+    nameView.setAttribute("class", "name_view")
+    nameView.innerHTML = player.name;
+
+    playerView.appendChild(cardView)
+    playerView.appendChild(nameView)
+
+    let gameView = document.getElementById("game_container")
+    gameView.appendChild(playerView)
+
+
+}
+
+
+
+
+
